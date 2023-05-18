089a6fc95fe8ab5409ebf8349f32aba2a3c5d522
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-05-18 15:22:44 -0400

    first commit

---
+++ carritoReact/src/data.js
@@ -0,0 +1,44 @@
+import mesa from '../assets/img/mesa.png';
+import sillas from '../assets/img/sillas.png';
+import taburete from '../assets/img/taburetes.png';
+import taburetes2 from '../assets/img/taburetes2.png';
+import mueble1 from '../assets/img/mueble1.png';
+
+export const data = [
+	{
+		id: 1,
+		img: mesa,
+		nameProduct: 'Mesa 1x2m.100%Pino',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: sillas,
+		nameProduct: 'Sillas',
+		price: 20,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: taburete,
+		nameProduct: 'Taburete con varniz maple',
+		price: 20,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: taburetes2,
+		nameProduct: 'Taburetes 50cm. 100%Pino',
+		price: 30,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: mueble1,
+		nameProduct: 'Mueble a pedido',
+		price: 50,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
